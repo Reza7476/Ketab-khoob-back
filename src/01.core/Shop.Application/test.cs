76eb29f04c348438bd46c3bbd1ea63c4800af480
Reza7476 <rdehghani.akorn@gmail.com>
2024-10-17 12:20:20 +0330

    new changes

--- src/01.core/Shop.Application/test.cs
+++ src/01.core/Shop.Application/test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,4 +9,7 @@ namespace Shop.Application;
 internal class test
 {
     public int MyProperty { get; set; }
+
+    public int reza { get; set; }
+
 }
